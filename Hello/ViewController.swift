05e674c37840019c674eb330ff394cd53516ac01
Karthik UK <karthikuk@Admins-Mac-mini-4.local>
2019-07-30 16:46:25 +0530

    First commit

---
+++ Hello/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Hello
+//
+//  Created by Karthik UK on 30/07/19.
+//  Copyright © 2019 Karthik UK. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("fgbnh")
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
